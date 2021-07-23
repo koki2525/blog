c165cd7d4d578e91fb11a0b304fa83544df7f07c
Rachael Maphopha <rachael.maphopha@clickatell.com>
2021-07-23 09:47:04 +0200

    Sedders
    Added Post and User seeders

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'test'
+        ]);
+    }
+}
